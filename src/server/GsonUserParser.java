38a36febc1213e58778993d19606e8c88c0a362d
Yuen Ka Hang Christian <2214959@slu.edu.ph>
2024-03-16 20:16:43 +0800

    added method [createAccount]

--- src/server/GsonUserParser.java
+++ src/server/GsonUserParser.java
@@ -64,12 +64,35 @@ public class GsonUserParser {
         return false;
     }
 
+    /**
+     * This method allows the program to insert values concerning new users to the Json file.
+     * To do this, ArrayList userArrayList is used to store arrays of current users stored in the
+     * server, and then parameters regarding new user would be combined into a variable called
+     * [User], before inserting it into an array and parse it into Json file
+     * @param firstName First name of the new user
+     * @param lastName last name of the new user
+     * @param username username of the new user
+     * @param phoneNumber phone number of the new user
+     * @param password encrypted password associated to the new user
+     * @return
+     */
+    public boolean createAccount(String firstName, String lastName, String username, String phoneNumber, String
+            password){
+        //params for user: String username, String type, String password, String lastName, String firstName, String phoneNumber
+        User newPerson  = new User(username, "user",password,lastName, firstName,phoneNumber);
+        userArrayList.add(newPerson);
 
+        //userArrayList 入落 json
+        this.updateUserList();
+        return false;
+    }
 
     public static void main(String[] args) {
         GsonUserParser parser = new GsonUserParser();
         User[] users = parser.getUsers();
-        System.out.print(users[0]);
+        //System.out.print(users[0]);
+        parser.createAccount("Yuen", "Ka Hang", "Perhaps", "09456123870","perhapas");
+        //System.out.print(users[1].getUsername());
     }
 
 
