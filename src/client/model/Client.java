2d59134ba207e234c106a0bb762599606bebaedd
Rithik Tank <2233293@slu.edu.ph>
2024-03-18 07:49:48 +0800

    added host file reading

--- src/client/model/Client.java
+++ src/client/model/Client.java
@@ -5,18 +5,31 @@ import client.view.LoginView;
 import shared.ServerMessage;
 
 import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Scanner;
 
 
 public class Client {
     ServerMessage remote;
     String username;
 
+    String host;
+
     public Client() throws RemoteException, NotBoundException{
-        Registry server = LocateRegistry.getRegistry(2000);
+        try {
+            Scanner reader = new Scanner(new File("src/client/host"));
+            host = reader.nextLine();
+            reader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Registry server = LocateRegistry.getRegistry(host, 2000);
         this.remote = (ServerMessage) server.lookup("server");
 
     }
